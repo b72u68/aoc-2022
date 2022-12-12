81717cb5725b00df9e008c38883fd636d54a2103
b72u68 <mdinh@hawk.iit.edu>
2022-12-11 23:05:09 -0600

    Solution for day 6
    - Should have used set or dictionary to optimize the sliding window but
      I was lazy

--- day6/day6.py
+++ day6/day6.py
@@ -1,26 +1,30 @@
 import os
 import sys
+from collections import defaultdict
 
 
 # get data from file
 def get_data(filename):
     file_dir = os.path.join(os.getcwd(), filename)
-    f = open(file_dir)
-
-    # process data here
-    data = f.readlines()
-
-    return data
+    with open(file_dir, "r") as f:
+        raw_data = f.readlines()
+    return raw_data[0].strip()
 
 
 # solution for part 1
 def part1(data):
-    return
+    for i in range(len(data)-4):
+        packet = data[i:i+4]
+        if len(set(packet)) == len(packet):
+            return i + 4
 
 
 # solution for part 2
 def part2(data):
-    return
+    for i in range(len(data)-14):
+        packet = data[i:i+14]
+        if len(set(packet)) == len(packet):
+            return i + 14
 
 
 if __name__ == "__main__":
