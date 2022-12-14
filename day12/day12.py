ab331ca979ea3164aa6521bc8d7c712fc5583ebb
b72u68 <mdinh@hawk.iit.edu>
2022-12-13 19:49:15 -0600

    Solution for day 12

--- day12/day12.py
+++ day12/day12.py
@@ -1,7 +1,7 @@
 import os
 import sys
 import copy
-from queue import PriorityQueue
+from collections import deque
 
 
 # get data from file
@@ -27,15 +27,40 @@ def get_data(filename):
     return (map, start, end)
 
 
+def bfs(map, start, end):
+    rows, cols = len(map), len(map[0])
+    q = deque([[start]])
+    visited = set([start])
+    while q:
+        path = q.popleft()
+        i, j = path[-1]
+        if (i, j) == end:
+            return path
+        for ni, nj in ((i+1, j), (i-1, j), (i, j+1), (i, j-1)):
+            if 0 <= ni < rows and 0 <= nj < cols and (ni, nj) not in visited:
+                if map[ni][nj] <= map[i][j] + 1:
+                    q.append(path + [(ni, nj)])
+                    visited.add((ni, nj))
+
+
 # solution for part 1
 def part1(data):
     map, start, end = copy.deepcopy(data)
-    return
+    path = bfs(map, start, end)
+    return len(path) - 1 if path else None
 
 
 # solution for part 2
 def part2(data):
-    return
+    map, _, end = copy.deepcopy(data)
+    minpath = float('inf')
+    for i in range(len(map)):
+        for j in range(len(map[i])):
+            if map[i][j] == 0:
+                path = bfs(map, (i, j), end)
+                if path:
+                    minpath = min(minpath, len(path)-1)
+    return minpath
 
 
 if __name__ == "__main__":
