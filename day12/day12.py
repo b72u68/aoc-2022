7a72d92eaaef88fafde94b34c21b8446890cf348
b72u68 <mdinh@hawk.iit.edu>
2022-12-13 19:35:21 -0600

    Solution for day 9

--- day12/day12.py
+++ day12/day12.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import copy
+from queue import PriorityQueue
 
 
 # get data from file
@@ -9,13 +11,25 @@ def get_data(filename):
         raw_data = [line.strip() for line in f.readlines()]
 
     # process data here
-    data = raw_data
-
-    return data
+    start = None
+    end = None
+    map = [[0 for _ in range(len(raw_data[0]))] for _ in range(len(raw_data))]
+    for i in range(len(raw_data)):
+        for j in range(len(raw_data[i])):
+            if raw_data[i][j] == "S":
+                map[i][j] = 0
+                start = (i, j)
+            elif raw_data[i][j] == "E":
+                map[i][j] = 25
+                end = (i, j)
+            else:
+                map[i][j] = ord(raw_data[i][j]) - ord("a")
+    return (map, start, end)
 
 
 # solution for part 1
 def part1(data):
+    map, start, end = copy.deepcopy(data)
     return
 
 
--- day9/day9.py
+++ day9/day9.py
@@ -6,22 +6,56 @@ import sys
 def get_data(filename):
     file_dir = os.path.join(os.getcwd(), filename)
     with open(file_dir, "r") as f:
-        raw_data = f.readlines()
-
-    # process data here
-    data = raw_data
-
-    return data
+        return [line.strip().split() for line in f.readlines()]
 
 
 # solution for part 1
 def part1(data):
-    return
+    tp = [[0,0]]
+    kl = [[0,0] for _ in range(9)]
+    H = [0,0]
+    t = kl[0]
+    dd = {'U':[1,1], 'D':[1,-1], 'R':[0,1], 'L':[0,-1]}
+    for line in data:
+        n = int(line[1])
+        e = dd[line[0]]
+        for i in range(n):
+            H[e[0]] += e[1]
+            p = list(H)
+            for i in kl:
+                xd = p[0] - i[0]
+                yd = p[1] - i[1]
+                if not (abs(xd) <=1 and abs(yd) <= 1):
+                    if xd != 0: i[0] += int(abs(xd)/xd)
+                    if yd != 0: i[1] += int(abs(yd)/yd)
+                p = list(i)
+                break
+            if list(t) not in tp: tp.append(list(t))
+    return len(tp)
 
 
 # solution for part 2
 def part2(data):
-    return
+    tp = [[0,0]]
+    kl = [[0,0] for _ in range(9)]
+    H = [0,0]
+    t = kl[8]
+    dd = {'U':[1,1], 'D':[1,-1], 'R':[0,1], 'L':[0,-1]}
+    for line in data:
+        n = int(line[1])
+        e = dd[line[0]]
+        for i in range(n):
+            H[e[0]] += e[1]
+            p = list(H)
+            for i in kl:
+                xd = p[0] - i[0]
+                yd = p[1] - i[1]
+                if not (abs(xd) <=1 and abs(yd) <= 1):
+                    if xd != 0: i[0] += int(abs(xd)/xd)
+                    if yd != 0: i[1] += int(abs(yd)/yd)
+                p = list(i)
+            if list(t) not in tp: tp.append(list(t))
+    return len(tp)
 
 
 if __name__ == "__main__":
