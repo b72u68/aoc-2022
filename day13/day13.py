85e1334d6b1939e92ff9f8cb67e1ee62bf259c66
b72u68 <mdinh@hawk.iit.edu>
2022-12-14 00:02:17 -0600

    Solution for day 13
    - Using bubblesort because I'm stupid
    - "Years of academy traning wasted!"

--- day13/day13.py
+++ day13/day13.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from functools import cmp_to_key
 
 
 # get data from file
@@ -7,21 +8,73 @@ def get_data(filename):
     file_dir = os.path.join(os.getcwd(), filename)
     with open(file_dir, "r") as f:
         raw_data = [line.strip() for line in f.readlines()]
-
-    # process data here
-    data = raw_data
-
-    return data
+    return [(eval(raw_data[i]), eval(raw_data[i+1])) for i in range(0, len(raw_data), 3)]
 
 
 # solution for part 1
+def compare(l1, l2, nested=0):
+    # print(f"{' '*nested*2}Compare {l1} vs {l2}")
+    i1 = i2 = 0
+    while i1 < len(l1) and i2 < len(l2):
+        # print(f"{' '*nested*2}Compare {l1[i1]} vs {l2[i2]}")
+        if isinstance(l1[i1], int) and isinstance(l2[i2], int):
+            if l1[i1] > l2[i2]:
+                return 0
+            elif l1[i1] < l2[i2]:
+                return 1
+        elif isinstance(l1[i1], int) and isinstance(l2[i2], list):
+            # print(f"{' '*(nested+1)*2}Mixed types; convert left to {[l1[i1]]} and retry comparison")
+            result = compare([l1[i1]], l2[i2], nested+1)
+            if result == 0 or result == 1:
+                return result
+        elif isinstance(l1[i1], list) and isinstance(l2[i2], int):
+            # print(f"{' '*(nested+1)*2}Mixed types; convert right to {[l2[i2]]} and retry comparison")
+            result = compare(l1[i1], [l2[i2]], nested+1)
+            if result == 0 or result == 1:
+                return result
+        else:
+            result = compare(l1[i1], l2[i2], nested+1)
+            if result == 0 or result == 1:
+                return result
+        i1 += 1
+        i2 += 1
+    if i1 < len(l1) and i2 >= len(l2):
+        return 0
+    elif i1 == len(l1) and i2 == len(l2):
+        return 2
+    return 1
+
 def part1(data):
-    return
+    count = 0
+    for i in range(len(data)):
+        l1, l2 = data[i]
+        result = compare(l1, l2)
+        if result:
+            count += i + 1
+    return count
 
 
 # solution for part 2
+def sort(packets):
+    # bubblesort because I'm stupid
+    while True:
+        swapped = False
+        for i in range(len(packets)-1):
+            if not compare(packets[i], packets[i+1]):
+                packets[i], packets[i+1] = packets[i+1], packets[i]
+                swapped = True
+        if not swapped:
+            break
+    return packets
+
 def part2(data):
-    return
+    packets = []
+    for i in range(len(data)):
+        packets.extend(data[i])
+    packets.append([[2]])
+    packets.append([[6]])
+    sorted_packets = sort(packets)
+    return (packets.index([[2]]) + 1) * (packets.index([[6]]) + 1)
 
 
 if __name__ == "__main__":
